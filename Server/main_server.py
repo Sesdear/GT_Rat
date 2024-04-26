d99cb3b112aa489927561987a36ed904daf47f19
TemanSv1n <temasyr.o@gmail.com>
2024-04-26 20:36:59 +0300

    goyda slon
    svo

--- Server/main_server.py
+++ Server/main_server.py
@@ -11,6 +11,11 @@ sock.listen(30)
 Threads = []
 Clients = [] #S ocket, address, UUID
 
+SuThreads = []
+SuClients = [] #S ocket, address, UUID
+
+
+
 """UTIL"""
 def checkClientDuplicates(TupleList: list, hostname: str):
     for i in TupleList:
@@ -26,11 +31,33 @@ def getSocketByHostname(list_of_tuples, search_string):
 def sendToClient(clientsocket: socket, command: str):
     clientsocket.send(bytes(command, encoding = 'utf-8'))
 
-def sendToClientByName(comp_name: str, command: str):
-    clientsocket = getSocketByHostname(Clients, comp_name)
+def sendToClientByName(comp_name: str, command: str, list_of_tuples: list):
+    clientsocket = getSocketByHostname(list_of_tuples, comp_name)
     clientsocket.send(bytes(command, encoding = 'utf-8'))
 
 def on_new_client(clientsocket,addr):
+    while True:
+        msg = clientsocket.recv(1024)
+        if msg == b"":
+            clientsocket.close()
+            return
+        else:
+            print(f"{msg}")
+            return msg
+
+        '''Some Handler Here'''
+
+def clientListener(clientsocket,addr):
+    while True:
+        msg = clientsocket.recv(1024)
+        if msg == b"":
+            clientsocket.close()
+            return
+        else:
+            print(f"{msg}")
+        '''Some Handler Here'''
+
+def suClientListener(clientsocket,addr):
     while True:
         msg = clientsocket.recv(1024)
         if msg == b"":
@@ -42,23 +69,43 @@ def on_new_client(clientsocket,addr):
 
 
 
+try:
+    while True:
+        conn, addr = sock.accept()
+        hostname = socket.gethostname()
+        print(hostname, 'connected:', addr)
 
-while True:
-    conn, addr = sock.accept()
-    hostname = socket.gethostname()
-    print(hostname, 'connected:', addr)
-    if not checkClientDuplicates(Clients, hostname):
-        Clients.append((conn, addr, hostname))
-        Threads.append(threading.Thread(target=on_new_client, args = (conn, addr)))
-        Threads[len(Threads)-1].start()
-        for index, i in enumerate(Threads):
-            if not i.is_alive():
-                Threads.pop(index)
-                Clients.pop(index)
-        print(Threads)
-        print(Clients)
-        sendToClientByName("DESKTOP-BO4V25B", "ABAS")
-    else:
-        print("Client PC ID duplicate")
+        msg = on_new_client(conn, addr)
+        if msg == b"client":
+            if not checkClientDuplicates(Clients, hostname):
+                Clients.append((conn, addr, hostname))
+                Threads.append(threading.Thread(target=clientListener, args = (conn, addr)))
+                Threads[len(Threads)-1].start()
+                for index, i in enumerate(Threads):
+                    if not i.is_alive():
+                        Threads.pop(index)
+                        Clients.pop(index)
+                print(Threads)
+                print(Clients)
+                sendToClientByName("DESKTOP-BO4V25B", "ABAS", Clients)
+            else:
+                print("DUPLICATE PC")
+        elif msg == b"sudo":
+            if not checkClientDuplicates(SuClients, hostname):
+                SuClients.append((conn, addr, hostname))
+                SuThreads.append(threading.Thread(target=suClientListener, args=(conn, addr)))
+                SuThreads[len(SuThreads) - 1].start()
+                for index, i in enumerate(Threads):
+                    if not i.is_alive():
+                        SuThreads.pop(index)
+                        SuClients.pop(index)
+                print(SuThreads)
+                print(SuClients)
+                sendToClientByName("DESKTOP-BO4V25B", "SUDO", SuClients)
+            else:
+                print("DUPLICATE SUDO PC")
+
+except socket.error or socket.timeout as e:
+    print("NUH UH")
 
 
--- SuperClient/main_superclient.py
+++ SuperClient/main_superclient.py
@@ -8,13 +8,13 @@ sock = socket.socket()
 sock.connect((IP, PORT))
 
 def enteringMessage():
-    message = "client"
+    message = "sudo"
     sock.send(bytes(message, encoding='utf-8'))
 
 enteringMessage()
 
-# data = sock.recv(1024)
+data = sock.recv(1024)
 #sock.close()
-#
-# print (data)
+
+print (data)
 input()
